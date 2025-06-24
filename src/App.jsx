5608ac6119b79bc06ada40e13dcf8677d49d7430
levithX <jorgehenriquemoraesbarreto@gmail.com>
2025-06-24 14:09:23 -0300

    feat: monstra resultado com o botao

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import EscolhaVotos from './componente/EscolhaVotos'
 
 
 function App() {
-const [votado, setVotado] = useState(false)
+const [ganhador, setGanhador] = useState(null)
+const [falhaResultado,setFalhaResultado ] = useState(false)
 const [listaDeVotacao, setListaDeVotacao] = useState([ 
     {id: 1,  title: "javascript",votos: 0},
     {id: 2,title: "python",votos: 0},
@@ -11,19 +12,46 @@ const [listaDeVotacao, setListaDeVotacao] = useState([
     {id: 4, title: "go", votos: 0 },  
     {id: 5,title: "c#",votos: 0}
 ])
+//function para o escolhaVotos 
 function onvotado(id ) { 
      const novaLista = listaDeVotacao.map(opcao => { 
             if (opcao.id === id) {
-        opcao.votos = opcao.votos + 1 
+        return {...opcao, votos: opcao.votos + 1}
       }
       console.log("nome:" + opcao.title + " numero de votos:" + opcao.votos)
       return opcao;
     })
     setListaDeVotacao(novaLista)
   }
+// function para o resultado 
+function mostraresultado() {
+  let comparacao = 1
+  let venceu = null
+  let algumZerado = false
+
+  listaDeVotacao.forEach(opcao => {
+    if(opcao.votos === 0) {
+      algumZerado = true
+    }
+    if(comparacao <= opcao.votos) {
+      comparacao = opcao.votos
+      venceu = opcao.title
+    }
+  })
+
+  if(algumZerado) {
+    setFalhaResultado(true)
+  } else {
+    setFalhaResultado(false)
+  }
+
+  setGanhador(venceu)
+}
   return (
     <div>
-      <EscolhaVotos listaDeVotacao={listaDeVotacao} onvotado={onvotado}/>
+      <EscolhaVotos listaDeVotacao={listaDeVotacao} onvotado={onvotado} />
+      <button onClick={mostraresultado}> monstra resultado</button>
+      {ganhador && !falhaResultado } <p>{ganhador}</p>
     </div>
   )
 }
--- src/componente/EscolhaVotos.jsx
+++ src/componente/EscolhaVotos.jsx
@@ -2,10 +2,11 @@ function EscolhaVotos({ listaDeVotacao, onvotado, votado }) {
   return (
     <div>
       {listaDeVotacao.map(opcao => (
-        <p key={opcao.id}>
-          <button onClick={() => onvotado(opcao.id)} disabled={votado}>
+            <p key={opcao.id}>
+          <button onClick={() => onvotado(opcao.id)} disabled={votado} >
             {opcao.title}
           </button>
+          <p>{opcao.votos}</p>
         </p>
       ))}
     </div>
