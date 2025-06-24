6bae44d493abf33bf276064f547fd71d96458ee2
levithX <jorgehenriquemoraesbarreto@gmail.com>
2025-06-24 14:14:17 -0300

    correcao: escolhaVotos

--- src/componente/EscolhaVotos.jsx
+++ src/componente/EscolhaVotos.jsx
@@ -2,12 +2,12 @@ function EscolhaVotos({ listaDeVotacao, onvotado, votado }) {
   return (
     <div>
       {listaDeVotacao.map(opcao => (
-            <p key={opcao.id}>
+            <div key={opcao.id}>
           <button onClick={() => onvotado(opcao.id)} disabled={votado} >
             {opcao.title}
           </button>
           <p>{opcao.votos}</p>
-        </p>
+        </div>
       ))}
     </div>
   )
